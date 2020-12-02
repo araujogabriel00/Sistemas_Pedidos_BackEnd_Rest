07a5d6b13a656b8bbc0cdde37bb5368c6b25201d
gabriel.araujos@sempreceub.com <gabriel.araujos@sempreceub.com>
2020-12-02 15:52:45 -0300

    Teste Resources

--- src/main/java/com/example/SpringBoot_Rest/resources/CategoriaResources.java
+++ src/main/java/com/example/SpringBoot_Rest/resources/CategoriaResources.java
@@ -1,16 +1,29 @@
 package com.example.SpringBoot_Rest.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.SpringBoot_Rest.entities.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResources {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando";
+	public List<Categoria> listar() {
+
+		Categoria cat = new Categoria(1, "Placa de Video");
+		Categoria cat1 = new Categoria(2, "Placa de Mãe");
+
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat);
+		lista.add(cat1);
+
+		return lista;
 	}
 
 }
